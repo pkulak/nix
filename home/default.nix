0b9de66095526ea1949f8738555b809087882895
Phil Kulak <phil@kulak.us>
2023-08-26 19:45:16 -0700

    XDG dirs

--- home/default.nix
+++ home/default.nix
@@ -170,6 +170,18 @@ in {
 
       application/pdf=org.pwmt.zathura-pdf-mupdf.desktop
     '';
+
+    # XDG Dirs
+    "user-dirs.dirs".text = ''
+      XDG_DESKTOP_DIR="$HOME/Desktop"
+      XDG_DOCUMENTS_DIR="$HOME/Documents"
+      XDG_DOWNLOAD_DIR="$HOME/Downloads"
+      XDG_MUSIC_DIR="$HOME/Music"
+      XDG_PICTURES_DIR="$HOME/Pictures"
+      XDG_PUBLICSHARE_DIR="$HOME/Public"
+      XDG_TEMPLATES_DIR="$HOME/Templates"
+      XDG_VIDEOS_DIR="$HOME/Videos"
+    '';
   };
 
   home.file = {
