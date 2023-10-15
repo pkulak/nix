b22ab76e4b93a27c57fbc1aa1561313f19640754
Phil Kulak <phil@kulak.us>
2023-10-14 17:46:34 -0700

    Intellij

--- hosts/x1/default.nix
+++ hosts/x1/default.nix
@@ -6,6 +6,10 @@
     nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
   ];
 
+  environment.systemPackages = [
+    pkgs.jetbrains.idea-ultimate
+  ];
+
   networking.hostName = "x1";
   programs.light.enable = true;
 }
