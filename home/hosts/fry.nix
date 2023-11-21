219a74cadcbce2b4affe63076a669077e65a8074
Phil Kulak <phil@kulak.us>
2023-11-21 12:02:46 -0800

    Go back to suspend on a timer.

--- home/hosts/fry.nix
+++ home/hosts/fry.nix
@@ -7,7 +7,9 @@
     Service.ExecStart = ''
         ${pkgs.swayidle}/bin/swayidle -w \
             timeout 600 'swaymsg "output * dpms off"' \
-                    resume 'swaymsg "output * dpms on"'
+                    resume 'swaymsg "output * dpms on"' \
+            timeout 7200 'systemctl suspend' \
+                    after-resume 'swaymsg "output * dpms on"'
       ''; 
 
     Service.Environment = "PATH=/bin:/run/current-system/sw/bin";
