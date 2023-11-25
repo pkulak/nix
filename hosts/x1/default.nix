5baab48601b83562467fe2436a161dd2d198156a
Phil Kulak <phil@kulak.us>
2023-11-25 13:18:10 -0800

    Add bluetooth to my laptop.

--- hosts/x1/default.nix
+++ hosts/x1/default.nix
@@ -6,6 +6,10 @@
     nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
   ];
 
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  services.blueman.enable = true;
+
   environment.systemPackages = [
     pkgs.jetbrains.idea-ultimate
   ];
