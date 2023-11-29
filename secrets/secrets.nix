300176b1815809dc55ffbeef5574d3b368a22d39
Phil Kulak <phil@kulak.us>
2023-11-29 09:44:24 -0800

    Add a machine.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,11 @@
 let
   user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBWOTXI/ryuoyQSepiKc+EF5lm+Ye3vqa2a5xS4pBA4C";
 
-  fry = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHT/UgnbVTNVHfcdGfnaFmRPwxTKtm8SZWVVV/3k/KDu";
-  x1  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqzqd5PgJylsmJdYAZi0xRXD+nF8SQxuQVZVOZS7H45";
+  fry    = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHT/UgnbVTNVHfcdGfnaFmRPwxTKtm8SZWVVV/3k/KDu";
+  x1     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqzqd5PgJylsmJdYAZi0xRXD+nF8SQxuQVZVOZS7H45";
+  t460p  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxLL9ihLEJdYgXX0qTtCBwexdC1ffA2Qh3wHPKhQHCM";
 
-  keys = [ user fry x1 ];
+  keys = [ user fry x1 t460p ];
 in {
   "smb-secrets.age".publicKeys = keys;
   "1pass.age".publicKeys = keys;
