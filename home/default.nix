55699d7b3698b58ee46ae8423d443199a52559d1
Phil Kulak <phil@kulak.us>
2023-11-30 08:50:16 -0800

    Add a Fish function.

--- home/default.nix
+++ home/default.nix
@@ -146,6 +146,18 @@ in {
       end
     '';
 
+    "fish/functions/frg.fish".text = ''
+      function frg --description "rg tui built with fzf and bat"
+        ${pkgs.ripgrep}/bin/rg --ignore-case --color=always --line-number --no-heading "$argv" |
+            ${pkgs.fzf}/bin/fzf --ansi \
+                --color 'hl:-1:underline,hl+:-1:underline:reverse' \
+                --delimiter ':' \
+                --preview "${pkgs.bat}/bin/bat --color=always {1} --theme='Solarized (light)' --highlight-line {2}" \
+                --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+                --bind "enter:become($EDITOR +{2} {1})"
+      end
+    '';
+
     # Mako
     "mako/config".text = "font=monospace 12";
 
