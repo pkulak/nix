971eb401fe9addfdb422efc1c8a87ffbdf17236a
Phil Kulak <phil@kulak.us>
2024-03-24 09:44:26 -0700

    Don't auto-save Markdown.

--- common/nixvim/config/plugins/auto-save.nix
+++ common/nixvim/config/plugins/auto-save.nix
@@ -1,1 +1,19 @@
-{ plugins.auto-save = { enable = true; }; }
+{
+  plugins.auto-save = {
+    enable = true;
+
+    condition = ''
+      function(buf)
+        local fn = vim.fn
+        local utils = require("auto-save.utils.data")
+        if
+          fn.getbufvar(buf, "&modifiable") == 1
+          and not utils.set_of({'md', 'markdown'})[fn.getbufvar(buf, "&filetype")]
+        then
+          return true -- met condition(s), can save
+        end
+        return false -- can't save
+      end
+    '';
+  };
+}
