8eab0eec79e604dc9cea6e490139e38fed8cc741
Phil Kulak <phil@kulak.us>
2025-04-04 14:00:13 -0700

    No more AI in Firefox

--- home/firefox.nix
+++ home/firefox.nix
@@ -113,6 +113,7 @@
           user_pref("privacy.clearOnShutdown.sessions", false);
           user_pref("privacy.history.custom", true);
           user_pref("gfx.webrender.compositor.force-enabled", true);
+          user_pref("browser.ml.chat.enabled", false);
           user_pref("geo.provider.network.url", 'data:application/json,{"location": {"lat": 45.568259, "lng": -122.631719}, "accuracy": 128.0}');
         '';
 
