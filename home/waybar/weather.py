b09f74888a7606f88c461a99ab92e97bd94529f4
Phil Kulak <phil@kulak.us>
2025-04-26 21:22:52 -0700

    Formatting

--- home/waybar/weather.py
+++ home/waybar/weather.py
@@ -5,45 +5,43 @@ import requests
 import json
 
 token = keyring.get_password("nix", "ha")
-headers = {'Authorization': f"Bearer {token}"}
+headers = {"Authorization": f"Bearer {token}"}
 
 url = "http://ha.home/api/states/sensor.temperature"
 response = requests.get(url, headers=headers)
-temp = round(float(response.json()['state']), 1)
+temp = round(float(response.json()["state"]), 1)
 
 url = "http://ha.home/api/states/sensor.precipitation_per_hour"
 response = requests.get(url, headers=headers)
-precip = round(float(response.json()['state']), 1)
+precip = round(float(response.json()["state"]), 1)
 
 url = "http://ha.home/api/states/sun.sun"
 response = requests.get(url, headers=headers)
-sun = response.json()['state']
+sun = response.json()["state"]
 
 url = "http://ha.home/api/states/sensor.illuminance"
 response = requests.get(url, headers=headers)
-lux = int(response.json()['state'])
+lux = int(response.json()["state"])
 
 icon = ""
 
 if precip == 0:
     if lux > 40000:
-        icon = u"\U0000f185"
-    elif sun == 'above_horizon':
-        icon = u"\U0000f0c2"
+        icon = "\U0000f185"
+    elif sun == "above_horizon":
+        icon = "\U0000f0c2"
     else:
-        icon = u"\U0000f186"
+        icon = "\U0000f186"
 elif precip < 3:
-    icon = u"\U0000f73d"
+    icon = "\U0000f73d"
 else:
-    icon = u"\U0000f740"
+    icon = "\U0000f740"
 
 precip_str = f"   {precip} "
 
 if precip == 0.0:
     precip_str = ""
 
-data = {
-    "text": f"{temp}° {precip_str}{icon}"
-}
+data = {"text": f"{temp}° {precip_str}{icon}"}
 
 print(json.dumps(data))
