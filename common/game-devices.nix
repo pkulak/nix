61aff55a6b59844be346e7e85a85d8c3f02e467c
Phil Kulak <phil@kulak.us>
2025-06-18 15:42:37 -0700

    Update the game devices spec.

--- common/game-devices.nix
+++ common/game-devices.nix
@@ -1,17 +1,18 @@
 {
   stdenvNoCC,
-  fetchFromGitLab,
+  fetchFromGitea,
   ...
 }:
 stdenvNoCC.mkDerivation {
   name = "game-devices";
-  version = "20230523";
+  version = "20250618";
 
-  src = fetchFromGitLab {
+  src = fetchFromGitea {
+    domain = "codeberg.org";
     owner = "fabiscafe";
     repo = "game-devices-udev";
-    rev = "eddda2a2f268df0e9e14a99564431f880f88c5a6";
-    sha256 = "sha256-0H2yQM5ZrSHl2/rLTB0pjaPI1ZyBxPRcl9CA83HxcBs=";
+    rev = "0060b94062f1af8b00c6a9d5496e054f986c3626";
+    sha256 = "sha256-b2NBgGpRQ2pQZYQgiRSAt0loAxq1NEByRHVkQQRDOj0=";
   };
 
   installPhase = ''
