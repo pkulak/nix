817fd947e6e48e5d9fe8e478a54e77501eb44d19
Phil Kulak <phil@kulak.us>
2025-07-11 10:34:25 -0700

    Formatting

--- home/jj/prompt.fish
+++ home/jj/prompt.fish
@@ -2,35 +2,35 @@
 
 # Are we in a jj repo?
 if not jj root --quiet &>/dev/null
-    return 1
+  return 1
 end
 
 # Generate prompt
 jj log --ignore-working-copy --no-graph --color always -r @ -T '
-    surround(
-        " (",
-        ")",
-        separate(
-            " ",
-            bookmarks.join(", "),
-            coalesce(
-                surround(
-                    "\"",
-                    "\"",
-                    if(
-                        description.first_line().substr(0, 24).starts_with(description.first_line()),
-                        description.first_line().substr(0, 24),
-                        description.first_line().substr(0, 23) ++ "…"
-                    )
-                ),
-                "~"
-            ),
-            change_id.shortest(),
-            commit_id.shortest(),
-            if(conflict, "(conflict)"),
-            if(empty, "(empty)"),
-            if(divergent, "(divergent)"),
-            if(hidden, "(hidden)"),
-        )
+  surround(
+    " (",
+    ")",
+    separate(
+      " ",
+      bookmarks.join(", "),
+      coalesce(
+        surround(
+          "\"",
+          "\"",
+          if(
+            description.first_line().substr(0, 24).starts_with(description.first_line()),
+            description.first_line().substr(0, 24),
+            description.first_line().substr(0, 23) ++ "…"
+          )
+        ),
+        "~"
+      ),
+      change_id.shortest(),
+      commit_id.shortest(),
+      if(conflict, "(conflict)"),
+      if(empty, "(empty)"),
+      if(divergent, "(divergent)"),
+      if(hidden, "(hidden)"),
     )
+  )
 '
