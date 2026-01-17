682c6fee4ef21fc21db15de0a2ff3784a9878b7d
Phil Kulak <phil@kulak.us>
2026-01-17 15:42:28 -0800

    Better CO2.

--- home/waybar/co2.py
+++ home/waybar/co2.py
@@ -7,10 +7,21 @@ import json
 token = os.environ['HA_KEY']
 headers = {"Authorization": f"Bearer {token}"}
 
-url = "http://ha.home/api/states/sensor.co2"
+url = "http://ha.home/api/states/sensor.apollo_air_co2"
 response = requests.get(url, headers=headers)
-co2 = int(float(response.json()["state"]))
+co2_down = int(float(response.json()["state"]))
 
-data = {"text": f"{co2}ppm"}
+url = "http://ha.home/api/states/sensor.awair_bunny_co2"
+response = requests.get(url, headers=headers)
+co2_up = int(float(response.json()["state"]))
+
+icon = "↑"
+co2 = co2_up
+
+if co2_down > co2_up:
+    icon = "↓"
+    co2 = co2_down
+
+data = {"text": f"{co2} {icon}"}
 
 print(json.dumps(data))
