10caf3a2c4dfbcaf7047e423245c0fbfd13a21aa
Phil Kulak <phil@kulak.us>
2026-04-08 18:27:13 -0700

    Upgrade Jai to v0.3.

--- modules/features/jai/nixos/jai.nix
+++ modules/features/jai/nixos/jai.nix
@@ -1,24 +1,28 @@
-{ acl
-, lib
-, fetchurl
-, pkg-config
-, gcc15Stdenv
-, util-linux
-, ...
+{
+  acl,
+  lib,
+  fetchurl,
+  pkg-config,
+  gcc15Stdenv,
+  util-linux,
+  ...
 }:
 gcc15Stdenv.mkDerivation rec {
   pname = "jai";
-  version = "0.2";
+  version = "0.3";
 
   src = fetchurl {
     url = "https://github.com/stanford-scs/jai/releases/download/v${version}/jai-${version}.tar.gz";
-    hash = "sha256-neFe7Mxx+kKSReMPCeON4cWdAil7vjRUVb2fqnVT9iI=";
+    hash = "sha256-3uGFsdsyG7Nl/D3NtCDU0+S8Je/KiRKMMiGuyCWDx6E=";
   };
 
   patches = [ ./overlay-fd.patch ];
 
   nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ util-linux acl ];
+  buildInputs = [
+    util-linux
+    acl
+  ];
 
   env.NIX_CFLAGS_COMPILE = "-Wno-unused-result";
   configureFlags = [ "--prefix=${placeholder "out"}" ];
@@ -29,7 +33,10 @@ gcc15Stdenv.mkDerivation rec {
     description = "Light-weight sandbox for AI agents";
     homepage = "https://jai.scs.stanford.edu/";
     license = licenses.gpl3;
-    platforms = [ "x86_64-linux" "aarch64-linux" ];
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
     mainProgram = "jai";
   };
 }
