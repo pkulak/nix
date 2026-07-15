a0edab0d66d3cd35eef39238ee4d2b8c943a2baf
Phil Kulak <phil@kulak.us>
2026-07-15 16:27:30 -0700

    Add a samba share.

--- modules/features/smb/nixos.nix
+++ modules/features/smb/nixos.nix
@@ -1,57 +1,66 @@
 { config, pkgs, ... }:
 
 let
-    automount_opts = ["x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=phil,gid=users,credentials=/run/user/1000/agenix/smb-secrets"];
-in {
+  automount_opts = [
+    "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=phil,gid=users,credentials=/run/user/1000/agenix/smb-secrets"
+  ];
+in
+{
   config = {
     environment.systemPackages = [ pkgs.cifs-utils ];
 
     fileSystems."/mnt/docker" = {
-        device = "//lilnas.home/docker";
-        fsType = "cifs";
-        options = automount_opts;
+      device = "//lilnas.home/docker";
+      fsType = "cifs";
+      options = automount_opts;
     };
 
     fileSystems."/mnt/music" = {
-        device = "//lilnas.home/music";
-        fsType = "cifs";
-        options = automount_opts;
+      device = "//lilnas.home/music";
+      fsType = "cifs";
+      options = automount_opts;
     };
 
     fileSystems."/mnt/nas" = {
-        device = "//lilnas.home/home";
-        fsType = "cifs";
-        options = automount_opts;
+      device = "//lilnas.home/home";
+      fsType = "cifs";
+      options = automount_opts;
     };
 
     fileSystems."/mnt/public" = {
-        device = "//lilnas.home/public";
-        fsType = "cifs";
-        options = automount_opts;
+      device = "//lilnas.home/public";
+      fsType = "cifs";
+      options = automount_opts;
     };
 
     fileSystems."/mnt/swap" = {
-        device = "//lilnas.home/swap";
-        fsType = "cifs";
-        options = automount_opts;
+      device = "//lilnas.home/swap";
+      fsType = "cifs";
+      options = automount_opts;
     };
 
     fileSystems."/mnt/video" = {
-        device = "//lilnas.home/video";
-        fsType = "cifs";
-        options = automount_opts;
+      device = "//lilnas.home/video";
+      fsType = "cifs";
+      options = automount_opts;
     };
 
     fileSystems."/mnt/photos" = {
-        device = "//lilnas.home/photos";
-        fsType = "cifs";
-        options = automount_opts;
+      device = "//lilnas.home/photos";
+      fsType = "cifs";
+      options = automount_opts;
     };
 
     fileSystems."/mnt/documents" = {
-        device = "//lilnas.home/documents";
-        fsType = "cifs";
-        options = automount_opts;
+      device = "//lilnas.home/documents";
+      fsType = "cifs";
+      options = automount_opts;
+    };
+
+    fileSystems."/mnt/copyparty" = {
+      device = "//lilnas.home/copyparty";
+      fsType = "cifs";
+      options = automount_opts;
     };
   };
 }
